819e735799d2c2e3bd37ff379b164c61e9a90627
kunk80110 <kunk80110@gmail.com>
2022-12-04 16:27:42 +0100

    buttons of UI was improved

--- scripts/changelang.js
+++ scripts/changelang.js
@@ -1,44 +1,82 @@
-let language = 'pl';
+//######                                                                                        DATA OBJECT
+const dataOfPages = {
+  mainPage: {
+    ua: {
+      title: 'Чарівні нігті - Головна',
+      header1: 'Головна',
+      intro: 'Нігті',
+      header2: '',
+      subtitleGel: '',
+      subtitlePudra: '',
+    },
+    pl: {
+      title: 'Magiczne paznokcie - Główna',
+      header1: 'Główna',
+      intro: 'paznokcie',
+      header2: '',
+      subtitleGel: '',
+      subtitlePudra: '',
+    },
+  },
+  gelPage: {
+    ua: {
+      title: 'Чарівні нігті - Гель',
+      header1: 'Що таке гель?',
+      intro: 'Гель - ...',
+      header2: '',
+    },
+    pl: {
+      title: 'Magiczne paznokcie - Żel',
+      header1: 'Co to jest żel?',
+      intro: 'Żel - ...',
+      header2: '',
+    },
+  },
+  pudraPage: {
+    ua: {
+      title: 'Чарівні нігті - Пудра',
+      header1: 'Що таке пудра?',
+      intro: 'Пудра - ...',
+      header2: '',
+    },
+    pl: {
+      title: 'Magiczne paznokcie - Proszek',
+      header1: 'Co to jest proszek?',
+      intro: 'Proszek - ...',
+      header2: '',
+    },
+  },
+};
+
+//######                                                                                        RENDER_PAGE_FUCNTION
 const headerOne = document.getElementsByTagName('h1');
 const phrase = document.getElementsByTagName('p');
 
+const renderPage = (lang, page) => {
+  if (page === 'main') {
+    headerOne[0].innerText = dataOfPages.mainPage[lang].header1;
+    phrase[0].innerText = dataOfPages.mainPage[lang].intro;
+  } else if (page === 'what-is-gel') {
+    headerOne[0].innerText = dataOfPages.gelPage[lang].header1;
+    phrase[0].innerText = dataOfPages.gelPage[lang].intro;
+  } else if (page === 'what-is-pudra') {
+    headerOne[0].innerText = dataOfPages.pudraPage[lang].header1;
+    phrase[0].innerText = dataOfPages.pudraPage[lang].intro;
+  } else {
+    console.log('page doesn`t exist');
+  }
+};
+
+//######                                                                                        CHANGE_LANG_FUNCTION
+//uses by buttons
 const changeLang = (lang) => {
   localStorage.setItem('language', lang);
   console.log(localStorage.getItem('language'));
   renderPage(localStorage.getItem('language'), localStorage.getItem('page'));
 };
 
-const renderPage = (lang, page) => {
-  if (lang === 'pl') {
-    if (page === 'main') {
-      headerOne[0].innerText = 'Głowna';
-      phrase[0].innerText = 'Pazury';
-    } else if (page === 'what-is-gel') {
-      headerOne[0].innerText = 'Co to jest gel';
-      phrase[0].innerText = 'Gel - ...';
-    } else if (page === 'what-is-pudra') {
-      headerOne[0].innerText = 'Co to jest pudra';
-      phrase[0].innerText = 'Pudra - ...';
-    } else {
-      console.log('page doesn`t exist');
-    }
-  } else if (lang === 'ua') {
-    if (page === 'main') {
-      headerOne[0].innerText = 'Головна';
-      phrase[0].innerText = 'Нігтики';
-    } else if (page === 'what-is-gel') {
-      headerOne[0].innerText = 'Що таке гель';
-      phrase[0].innerText = 'Пудра - ...';
-    } else if (page === 'what-is-pudra') {
-      headerOne[0].innerText = 'Що таке пудра';
-      phrase[0].innerText = 'Гель - ...';
-    } else {
-      console.log('page doesn`t exist');
-    }
-  } else {
-  }
-};
-
+//######                                                                                        START_FUNCTION
+//uses by event onload
 const start = (page) => {
   localStorage.setItem('page', page);
   if (!localStorage.getItem('language')) {
@@ -46,5 +84,25 @@ const start = (page) => {
     renderPage(localStorage.getItem('language'), localStorage.getItem('page'));
   } else {
     renderPage(localStorage.getItem('language'), localStorage.getItem('page'));
+  }
+};
+
+//######                                                                                        HIDE/SHOW OTHER BUTTONS
+let switcher = 0;
+
+const hideShowBtns = () => {
+  const btnUP = document.getElementById('uptoup');
+  const btnUA = document.getElementById('ua');
+  const btnPL = document.getElementById('pl');
+  if (switcher === 0) {
+    btnPL.style.display = 'none';
+    btnUA.style.display = 'none';
+    btnUA.style.display = 'none';
+    switcher = 1;
+  } else {
+    btnPL.style.display = 'block';
+    btnUA.style.display = 'block';
+    btnUA.style.display = 'block';
+    switcher = 0;
   }
 };
