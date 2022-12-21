061e00bbaab38269c538898ac834604f23ad1e37
kunk80110 <kunk80110@gmail.com>
2022-12-21 16:58:39 +0100

    hehe

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -4,6 +4,7 @@ const preloadImages = (count, path) => {
   for (let i = 1; i == count; i++) {
     image = new Image();
     image.src = `imgs/${path}/${i}.jpg`;
+    console.log(`imgs/${path}/${i}.jpg`);
   }
 };
 
